7ca65ab217f3b0f2742006c60c0034b4af93146b
michalbubula <michal.bubula@outlook.com>
2024-08-19 15:24:08 +0200

    Adding parameters and code refactoring

--- json2label.py
+++ json2label.py
@@ -1,41 +1,36 @@
 import click
 import json
+import os
 from json_utils import *
 from tqdm import tqdm
 
-@click.group()
-def main():
-    pass
 
-@main.command()
+@click.command()
 @click.option(
-    "--dir_json",
-    "-dj",
-    help="directory of GT JSON files",
-    type=click.Path(exists=True, file_okay=False),
+    "--dir_in_json",
+    "-di",
+    help = "directory of GT JSON files",
+    type = click.Path(exists = True, file_okay = False),
 )
-
 @click.option(
     "--dir_out_labels",
     "-do",
-    help="directory where the output lables will be written in the png format.",
-    type=click.Path(exists=True, file_okay=False),
+    help = "directory where the output lables will be written in the png format.",
+    type = click.Path(exists = True, file_okay = False),
 )
-
 @click.option(
     "--type_output",
     "-to",
-    help="this defines if the output should be 3d (encoded with RGB color for visibility) or 2d (used for training a model). Just pass '2d' or '3d'.",
+    help = "this defines if the output should be 3d (encoded with RGB color for visibility) or 2d (used for training a model). Just pass '2d' or '3d'.",
 )
 
-
-def labelme2png(dir_json, dir_out_labels, type_output):
-    ls_jsons = os.listdir(dir_json)
+def main(dir_in_json, dir_out_labels, type_output):
+    ls_jsons = os.listdir(dir_in_json)
     
     for ind in tqdm(ls_jsons):
-        json_name = os.path.join(dir_json, ind)
+        json_name = os.path.join(dir_in_json, ind)
         json_converter(json_name, dir_out_labels, type_output)
-    
+        
     
 if __name__ == "__main__":
     main()
--- json_utils.py
+++ json_utils.py
@@ -1,55 +1,69 @@
-import json
-import numpy as np
-import cv2
-import os
+import json  
+import numpy as np  
+import cv2  
+import os  
 from tqdm import tqdm
 
-
 def json_converter(json_dir, png_out, type_out):
-    f = open(json_dir)
-    data = json.load(f)
-    size = {
-        "height": int(data["imageHeight"]),
-        "width": int(data["imageWidth"])
-    }
-    image = np.zeros((size["height"], size["width"], 3), np.uint8)
-    image[:, :] = (0, 0, 0) #background color
-    color2d = (1,1,1)
-    color3d = {
-        "paragraph": (255, 0, 0),
-        "header": (0, 255, 255),
-        "pagenumber": (0, 255, 0),
-        "dropcapital": (0, 0, 255)
+    colors = {
+        "2d": {
+            "paragraph": (1, 1, 1),
+            "header": (2, 2, 2),
+            "pagenumber": (3, 3, 3),
+            "dropcapital": (4, 4, 4),
+            "footnote": (5, 5, 5),
+            "footnote-continued": (6, 6, 6),
+            "endnote": (7, 7, 7),
+            "signature-mark": (8, 8, 8),
+            "catch-word": (9, 9, 9),
+            "marginalia": (10, 10, 10),
+            "TOC-entry": (11, 11, 11),
+            "floating": (12, 12, 12),
+            "imageregion": (13, 13, 13),
+            "separatorregion": (14, 14, 14),
+            "graphicregion": (15, 15, 15),
+            "tableregion": (16, 16, 16)
+        },
+        "3d": {
+            "paragraph": (255, 0, 0),
+            "header": (0, 255, 255),
+            "pagenumber": (0, 255, 0),
+            "dropcapital": (0, 0, 255),
+            "footnote": (125, 0, 0),
+            "footnote-continued": (125, 125, 0),
+            "endnote": (125, 125, 75),
+            "signature-mark": (125, 125, 125),
+            "catch-word": (0, 125, 0),
+            "marginalia": (0, 0, 125),
+            "TOC-entry": (0, 125, 125),
+            "floating": (125, 0, 125),
+            "imageregion": (255, 0, 255),
+            "separatorregion": (255, 125, 0),
+            "graphicregion": (255, 0, 125),
+            "tableregion": (255, 125, 125)
+        }
     }
     
-    for key in data["shapes"]:        
-        points = np.array(key["points"], dtype=np.int32)
+    with open(json_dir) as f:
+        data = json.load(f)
+        size = {
+            "height": int(data["imageHeight"]),
+            "width": int(data["imageWidth"])
+        }
+        # Initialize a black background image
+        image = np.zeros((size["height"], size["width"], 3), np.uint8)
         
-        if key['label'] == "paragraph":
-            if type_out == "3d":
-                cv2.fillPoly(image, pts = [points], color = color3d["paragraph"])
-            elif type_out == "2d":
-                cv2.fillPoly(image, pts = [points], color = color2d)
-                
-        if key['label'] == "header":
-            if type_out == "3d":
-                cv2.fillPoly(image, pts = [points], color = color3d["header"])
-            elif type_out == "2d":
-                cv2.fillPoly(image, pts = [points], color = color2d)
-                
-        if key['label'] == "pagenumber": 
-            if type_out == "3d":
-                cv2.fillPoly(image, pts = [points], color = color3d["pagenumber"])
-            elif type_out == "2d":
-                cv2.fillPoly(image, pts = [points], color = color2d)
-                
-        if key['label'] == "dropcapital": 
-            if type_out == "3d":
-                cv2.fillPoly(image, pts = [points], color = color3d["dropcapital"])    
-            elif type_out == "2d":
-                cv2.fillPoly(image, pts = [points], color = color2d)
+        for key in data["shapes"]:
+            points = np.array(key["points"], dtype=np.int32)
             
-    json_tail = os.path.split(json_dir)
-    json_name = json_tail[1].split('.')[0]
-    png_name = os.path.join(png_out, json_name + ".png")
-    cv2.imwrite(png_name, image)
+            # Get the label and check if it exists in colors  
+            label = key["label"]
+            if label in colors[type_out.lower()]:  # Check for 2d or 3d  
+                cv2.fillPoly(image, pts=[points], color=colors[type_out.lower()][label])  # Draw the filled polygon
+
+        # Save the output image  
+        json_tail = os.path.split(json_dir)
+        json_name = json_tail[1].split(".")[0]
+        png_name = os.path.join(png_out, json_name + ".png")
+        cv2.imwrite(png_name, image)
+        
